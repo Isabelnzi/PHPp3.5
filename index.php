972ced644990e926a8dab23c9edfa9ea30ef8682
Nzingani <isabelnzingani@outlook.com>
2018-07-27 11:11:36 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>P3ex5</title>
+  </head>
+  <body>
+    <?php
+    $not= 4;
+while($not < 15)
+{
+echo 'on y arrive presque';
+$not += 15;
+}
+  ?>
+</body>
+</html>
